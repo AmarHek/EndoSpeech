574ac4c6d76d891ea81f3b9ffe3d005118c0d452
amh12ry <amar.hekalo@uni-wuerzburg.de>
2021-10-26 15:05:05 +0200

    update records controller

--- backend/src/controllers/record.controller.ts
+++ backend/src/controllers/record.controller.ts
@@ -1,27 +1,27 @@
 import Record from '../models/record.schema';
 import { Request, Response, NextFunction } from 'express';
 
-export function addRecord(req: Request, res: Response, next: NextFunction): void {
+export function addRecord(req: Request, res: Response): void {
     const record = new Record( {
         sessionID: req.body.sessionID,
         timestamp: req.body.timestamp,
         content: req.body.content
     });
     record.save().then(result => {
-        res.status(201).json({
+        res.status(201).send({
             message: "Records file saved successfully",
             postId: result._id
         });
     });
 }
 
-export function getRecordsByID(req: Request, res: Response, next: NextFunction): void {
+export function getRecordsByID(req: Request, res: Response): void {
     Record.find({ sessionID: req.body.sessionID }).exec(
         (err, records) => {
             if (err) {
                 res.status(500).send({message: err});
             }
-            res.status(200).json({
+            res.status(200).send({
                 message: "Records fetched",
                 records: records
             });
