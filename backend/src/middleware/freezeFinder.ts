e2dbf9b737bfd5437d17b6a7404fbca217b2da79
amh12ry <amar.hekalo@uni-wuerzburg.de>
2021-11-05 12:39:28 +0100

    fixed error messages for freezeFinder and fixed time direction of image vs freeze timestamps

--- backend/src/middleware/freezeFinder.ts
+++ backend/src/middleware/freezeFinder.ts
@@ -4,16 +4,18 @@ import * as fs from "fs";
 import * as Path from "path";
 
 let IMAGE_DIR: string;
+let DIR_PATIENCE: number;
 if (process.env.NODE_ENV === "production") {
     IMAGE_DIR = "\\\\Vcwdapp\\storage$\\SHORTTERM\\0\\0\\";
+    DIR_PATIENCE = 7200; // 2 hours
 } else {
     IMAGE_DIR = Path.join(__dirname, "../../data/examples/");
+    DIR_PATIENCE = 10000000; // very high number
 }
-
-// what is the patience for
-const DIR_PATIENCE = 7200; // 2 hours
 const IMAGE_PATIENCE = 120 // 2 minutes
-
+// DIR_PATIENCE makes sure that only directories from the past two hours are chosen
+// IMAGE_PATIENCE makes sure that a match only counts within 2 minutes of image creation
+// Image to text match works only in one direction (i.e. image is created before text)
 
 export function findDirectory(req: Request, res: Response, next: NextFunction) {
     const records = req.body.records;
@@ -27,16 +29,19 @@ export function findDirectory(req: Request, res: Response, next: NextFunction) {
     }
     for (const dir of possibleDirectories) {
         const matches = countMatches(records, dir);
+        console.log(dir, matches)
         if (matches > bestMatches) {
             rightDirectory = dir;
             bestMatches = matches;
             sameCounter = 0;
-        } else if (matches === bestMatches) {
+        } else if (matches === bestMatches && bestMatches !== 0) {
             sameCounter += 1;
         }
     }
-    if (sameCounter > 0) {
-        res.status(500).send({message: "Mehrere mögliche Ordner gefunden. Bitte manuell auswählen."});
+    if (bestMatches === 0) {
+        res.status(500).send({message: "Keinen passenden Ordner gefunden. Bitte manuell auswählen."});
+    } else if (sameCounter > 0) {
+        res.status(500).send({message: (sameCounter + 1) + " mögliche Ordner gefunden. Bitte manuell auswählen."});
     } else if (rightDirectory.length > 0) {
         freezes = fs.readdirSync(Path.join(IMAGE_DIR, rightDirectory));
         req.body.freezes = freezes;
@@ -104,7 +109,10 @@ function countMatches(records: any, directory: string): number {
         for (const record of records) {
             for (const freeze of freezes) {
                 testTime = Math.round(+fs.statSync(Path.join(IMAGE_DIR, directory, freeze)).mtime / 1000);
-                if (Math.abs(testTime - record.timestamp) <= IMAGE_PATIENCE) {
+                const diff = record.timestamp - testTime;
+                // diff > 0: testTime must be before record.timestamp
+                // diff <= IMAGE_PATIENCE: record must be created within IMAGE_PATIENCE after freeze timestamp
+                if (diff <= IMAGE_PATIENCE && diff > 0) {
                     nMatches += 1;
                     break;
                 }
