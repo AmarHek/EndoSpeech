415fcea1b8aa50ddf4cbe804e730f4f36aaf35d5
amh12ry <amar.hekalo@uni-wuerzburg.de>
2021-11-18 18:59:48 +0100

    update package.json and changed image_dir for test env

--- backend/src/controllers/freeze.controller.ts
+++ backend/src/controllers/freeze.controller.ts
@@ -5,9 +5,11 @@ import fs from "fs";
 
 let IMAGE_DIR: string;
 if (process.env.NODE_ENV === "development") {
+    console.log("it worked");
     IMAGE_DIR = Path.join(__dirname, "../../data/examples/");
 } else {
-    IMAGE_DIR = "\\\\Vcwdapp\\storage$\\SHORTTERM\\0\\0\\";
+    // IMAGE_DIR = "\\\\Vcwdapp\\storage$\\SHORTTERM\\0\\0\\";
+    IMAGE_DIR = Path.join(__dirname, "../../data/examples/")
 }
 
 export function addFreeze(req: Request, res: Response): void {
